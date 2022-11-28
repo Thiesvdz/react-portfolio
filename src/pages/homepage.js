b7252a8b7624064bccf75f3208bb39d4d3e6f6a7
Thies <Thiesvanderzon@gmail.com>
2022-11-28 10:52:43 +0100

    setup project

---
+++ src/pages/homepage.js
@@ -0,0 +1,11 @@
+import Header from "../components/Home/Header/Header"
+import About from "../components/Home/About/About"
+
+const homepage = () => {
+    return(
+        <Header />
+
+    )
+}
+
+export default homepage
