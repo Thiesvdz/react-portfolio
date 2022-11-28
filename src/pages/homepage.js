5e9423ad9f542ee6b93c47827dd58e009365235b
Thies <Thiesvanderzon@gmail.com>
2022-11-28 12:48:57 +0100

    begin header en navigation

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -1,10 +1,18 @@
+import Navigation from "../components/Home/Navigation/Navigation"
 import Header from "../components/Home/Header/Header"
 import About from "../components/Home/About/About"
+import Skills from "../components/Home/Skills/Skills"
+import Projects from "../components/Home/Skills/Skills"
 
 const homepage = () => {
     return(
-        <Header />
-
+        <>
+            <Navigation/>
+            <Header />
+            <About />
+            {/*<Skills />
+            <Projects /> */}
+        </>
     )
 }
 
