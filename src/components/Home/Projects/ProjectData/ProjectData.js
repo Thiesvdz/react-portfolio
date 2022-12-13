6a69f1904fccc62ae66552b71ed6df17c596a2d5
Thies <Thiesvanderzon@gmail.com>
2022-12-13 20:31:29 +0100

    project cards gemaakt met animatie

---
+++ src/components/Home/Projects/ProjectData/ProjectData.js
@@ -0,0 +1,39 @@
+import TEST from "../../../../images/Projects/coming.webp"
+
+
+const ProjectData = [
+    {
+        image: TEST,
+        classNameArticle: "article__1",
+        classNameHeader: "header__1",
+        icons1: "icons icons__1 fa-solid fa-globe",
+        icons2: "icons icons__1 fa-brands fa-trello",
+        icons3: "icons icons__1 fa-brands fa-github",
+    },
+    {
+        image: TEST,
+        classNameArticle: "article__2",
+        classNameHeader: "header__2",
+        icons1: "icons icons__2 fa-solid fa-globe",
+        icons2: "icons icons__2 fa-brands fa-trello",
+        icons3: "icons icons__2 fa-brands fa-github",
+    },
+    {
+        image: TEST,
+        classNameArticle: "article__3",
+        classNameHeader: "header__3",
+        icons1: "icons icons__3 fa-solid fa-globe",
+        icons2: "icons icons__3 fa-brands fa-trello",
+        icons3: "icons icons__3 fa-brands fa-github",
+    },
+    {
+        image: TEST,
+        classNameArticle: "article__4",
+        classNameHeader: "header__4",
+        icons1: "icons icons__4 fa-solid fa-globe",
+        icons2: "icons icons__4 fa-brands fa-trello",
+        icons3: "icons icons__4 fa-brands fa-github",
+    },
+]
+
+export default ProjectData
--- src/components/Home/Projects/Projects.js
+++ src/components/Home/Projects/Projects.js
@@ -1,5 +1,31 @@
+import ProjectData from "./ProjectData/ProjectData";
+import "./Projects.css"
+
 const Projects = () => {
-    return ( <h1>adfds</h1> );
-}
- 
+  let projectCardToBeRenderd = ProjectData.map((data) => {
+    return (
+      <article className= {data.classNameArticle + " project__article"} >
+        <header className={data.classNameHeader + " project__header"}>
+          <figure className="project__figure">
+            <img src={data.image} alt="" className="project__image" />
+          </figure>
+        </header>
+        <footer className="project__footer">
+          <ul className="project__ul">
+            <i className={data.icons1}><a href=""></a></i>
+            <i className={data.icons2}></i>
+            <i className={data.icons3}></i>
+          </ul>
+        </footer>
+      </article>
+    );
+  });
+
+  return <section className="project__section">
+      <div className="project__wrapper">
+        {projectCardToBeRenderd}
+      </div>
+      </section>;
+};
+
 export default Projects;
