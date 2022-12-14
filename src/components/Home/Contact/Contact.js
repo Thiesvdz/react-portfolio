b68009c50b759327d92f39118ad67c3e170abd85
Thies <Thiesvanderzon@gmail.com>
2022-12-14 14:47:18 +0100

    begin contact page

--- src/components/Home/Contact/Contact.js
+++ src/components/Home/Contact/Contact.js
@@ -1,6 +1,32 @@
+import "./Contact.css"
+
 const Contact = () => {
     return ( 
-        <h1>Contact</h1> 
+        <section className="contact__section">
+            <form action="" className="contact__form">
+                <div className="row">
+                    <div className="input__wrapper">
+                        <input type="text" id="name" className="input row" required/>
+                        <label htmlFor="name"><i class="fa-regular fa-user"></i> Naam</label>
+                    </div>
+                    <div className="input__wrapper">
+                    <input type="text" id="lastName" className="input row" required/>
+                        <label htmlFor="lastName"><i class="fa-regular fa-user"></i> Achternaam</label>
+                    </div>
+                </div>
+                <div className="input__wrapper">
+                    <input type="text" id="email" className="input" required/>
+                    <label htmlFor="email"><i class="fa-regular fa-envelope"></i> Email</label>
+                </div>
+                <div className="input__wrapper">
+                    <textarea name="" id="bericht" className="textarea" rows="8" required></textarea>
+                    <label htmlFor="bericht"><i class="fa-regular fa-comments"></i> Bericht</label>
+                </div>
+                <div className="btn__wrapper">
+                    <button className="contact__btn">VERSTUREN</button>
+                </div>
+            </form>
+        </section>
     );
 }
  
--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -3,7 +3,7 @@ import Header from "../components/Home/Header/Header"
 import About from "../components/Home/About/About"
 import Skills from "../components/Home/Skills/Skills"
 import Projects from "../components/Home/Projects/Projects"
-
+import Contact from "../components/Home/Contact/Contact"
 const homepage = () => {
     return(
         <>
@@ -23,6 +23,7 @@ const homepage = () => {
             />
             <Skills />
             <Projects />
+            <Contact />
         </>
     )
 }
