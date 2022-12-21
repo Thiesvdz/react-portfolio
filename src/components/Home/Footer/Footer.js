8bf5c8d4d51c817a1492007640bbd2fc28040c71
Thies <Thiesvanderzon@gmail.com>
2022-12-21 10:00:58 +0100

    begin footer

--- src/components/Home/Footer/Footer.js
+++ src/components/Home/Footer/Footer.js
@@ -9,7 +9,14 @@ const Footer = (props) => {
             <header className="footer__header">
                 <h1 className="footer__header__h1">NAVIGATIE</h1>
             </header>
-            <InfoWrapper/>
+            <InfoWrapper 
+                info__h3__1={"Contact"}
+                info__p__1={"06 12 34 56 78"}
+                info__h3__2={"E-mail"}
+                info__p__2={"thiesvanderzon@gmail.com"}
+                info__h3__3={"Locatie"}
+                info__p__3={"Warmenhuizen"}
+            />
             <NavWrapper />
             <SocialWrapper/>
         </footer>
--- src/components/Home/Footer/Wrappers/InfoWrapper.js
+++ src/components/Home/Footer/Wrappers/InfoWrapper.js
@@ -1,17 +1,20 @@
-const InfoWrapper = () => {
+const InfoWrapper = (props) => {
+
   return (
     <div className="footer__contact__info__wrapper">
       <ul className="footer__contact__info__ul">
-        <h3 className="footer__contact__info__h3">h3</h3>
-        <p className="footer__contact__info__p">asdsadsadsad</p>
-      </ul>
-      <ul className="footer__contact__info__ul">
-        <h3 className="footer__contact__info__h3">h3</h3>
-        <p className="footer__contact__info__p">asdsadsadsad</p>
-      </ul>
-      <ul className="footer__contact__info__ul">
-        <h3 className="footer__contact__info__h3">h3</h3>
-        <p className="footer__contact__info__p">asdsadsadsad</p>
+        <li className="footer__contact__info__li">
+          <h3 className="footer__contact__info__h3">{props.info__h3__1}</h3>
+          <p className="footer__contact__info__p">{props.info__p__1}</p>
+        </li>
+        <li className="footer__contact__info__li">
+          <h3 className="footer__contact__info__h3">{props.info__h3__2}</h3>
+          <p className="footer__contact__info__p">{props.info__p__2}</p>
+        </li>
+        <li className="footer__contact__info__li">
+          <h3 className="footer__contact__info__h3">{props.info__h3__3}</h3>
+          <p className="footer__contact__info__p">{props.info__p__3}</p>
+        </li>
       </ul>
     </div>
   );
