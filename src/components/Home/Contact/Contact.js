e5e03f175f36061774dc08d5a4bf33cec89bdf2a
Thies <Thiesvanderzon@gmail.com>
2022-12-21 15:21:46 +0100

    contact form redesign || nog niet responsive

--- src/components/Home/Contact/Contact.js
+++ src/components/Home/Contact/Contact.js
@@ -1,31 +1,15 @@
 import "./Contact.css"
+import ContactHeader from "./ContactSecties/ContactHeader"
+import ContactForm from "./ContactSecties/ContactForm"
 
 const Contact = () => {
     return ( 
         <section className="contact__section">
-            <form action="" className="contact__form">
-                <div className="row">
-                    <div className="input__wrapper">
-                        <input type="text" id="name" className="input row" required/>
-                        <label htmlFor="name"><i class="fa-regular fa-user"></i> Naam</label>
-                    </div>
-                    <div className="input__wrapper">
-                    <input type="text" id="lastName" className="input row" required/>
-                        <label htmlFor="lastName"><i class="fa-regular fa-user"></i> Achternaam</label>
-                    </div>
+            <ContactHeader/>
+            <ContactForm/ >
+            <div className="btn__wrapper">
+                    <button className="contact__btn">verstuur bericht</button>
                 </div>
-                <div className="input__wrapper">
-                    <input type="text" id="email" className="input" required/>
-                    <label htmlFor="email"><i class="fa-regular fa-envelope"></i> Email</label>
-                </div>
-                <div className="input__wrapper">
-                    <textarea name="" id="bericht" className="textarea" rows="8" required></textarea>
-                    <label htmlFor="bericht"><i class="fa-regular fa-comments"></i> Bericht</label>
-                </div>
-                <div className="btn__wrapper">
-                    <button className="contact__btn">VERSTUREN</button>
-                </div>
-            </form>
         </section>
     );
 }
---
+++ src/components/Home/Contact/ContactSecties/ContactForm.js
@@ -0,0 +1,42 @@
+const ContactHeader = () => {
+  return (
+    <form action="" className="contact__form">
+      <div className="row">
+        <div className="input__wrapper">
+          <input type="text" id="name" className="input row" required />
+          <label htmlFor="name">
+            <i class="fa-regular fa-user"></i> Naam
+          </label>
+        </div>
+        <div className="input__wrapper">
+          <input type="text" id="lastName" className="input row" required />
+          <label htmlFor="lastName">
+            <i class="fa-regular fa-user"></i> Achternaam
+          </label>
+        </div>
+
+        <div className="input__wrapper">
+          <input type="text" id="email" className="input" required />
+          <label htmlFor="email">
+            <i class="fa-regular fa-envelope"></i> Email
+          </label>
+        </div>
+      </div>
+
+      <div className="input__wrapper">
+        <textarea
+          name=""
+          id="bericht"
+          className="textarea"
+          rows="8"
+          required
+        ></textarea>
+        <label htmlFor="bericht">
+          <i class="fa-regular fa-comments"></i> Bericht
+        </label>
+      </div>
+    </form>
+  );
+};
+
+export default ContactHeader;
